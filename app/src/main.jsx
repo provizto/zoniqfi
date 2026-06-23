3b8605d4820040c76abfc97f0d702b66f91d25c1
Provizto <provizto@gmail.com>
2026-06-23 23:04:22 +0800

    Fix: inject robust external CDN buffer polyfill to head

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,3 +1,7 @@
+if (typeof window !== 'undefined') {
+  window.global = window;
+  window.process = { env: {} };
+}
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
