44cfe433971d206c32b760b04784200acda658ec
Provizto <provizto@gmail.com>
2026-06-23 21:32:08 +0800

    Update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Mengaktifkan polyfills untuk modul spesifik yang dibutuhkan Solana Web3 SDK
+      globals: {
+        Buffer: true, // Sangat krusial untuk transaksi dan keypair Solana
+        process: true,
+      },
+    }),
+  ],
 })
